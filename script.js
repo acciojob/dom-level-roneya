a9728c92efa80c364ffbbc93d5fe42c7c7fc41b9
Rohan Vidhate <49265832+roneya@users.noreply.github.com>
2023-04-14 19:03:29 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,2 +1,13 @@
 //your JS code here. If required.
-alert("The level of the element is: 5")
+const tags = document.querySelectorAll('li');
+let i = -1;
+
+tags.forEach((tag, index) => {
+  if (tag.getAttribute('id') === 'level') {
+    i = index;
+	alert(`The level of the element is: ${i+1}`)
+  }
+	
+});
+
+
